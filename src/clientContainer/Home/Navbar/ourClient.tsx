b6498c3635a6b801e35ffc45a039095f48513f31
Mirafzal1307 <usmonjonovmirafzal0713@gmail.com>
2022-06-04 10:59:55 +0500

    no

--- src/clientContainer/Home/Navbar/ourClient.tsx
+++ src/clientContainer/Home/Navbar/ourClient.tsx
@@ -16,8 +16,7 @@ const useStyles = makeStyles((theme) => ({
     cursor: "pointer",
     height: "calc(((100% + 110px) / 3) - 15px)",
     [theme.breakpoints.up(600)]: {
-      // width: "60% !important",
-      height: "calc(((100% + 130px) / 3) - 15px)",
+          height: "calc(((100% + 130px) / 3) - 15px)",
     },
     "&:hover": {
       opacity: "0.9 !important",
